ddc0d8fa8e0650f5acdf8b6e3c01e384ee3e864b
Sarah Fringer <sarah_fringer@brown.edu>
2018-11-28 16:00:03 -0500

    removed numbers again

--- clean_tweets.R
+++ clean_tweets.R
@@ -4,6 +4,6 @@ clean_tweets <- function(file) {
   library(stringr)
   tweets <- read.csv("coffee_tweets.csv")
   tweets <- select(tweets, text)
-  tweets_clean <- gsub("\\d","", tweets)
+  tweets_clean <- gsub("\\d","", tweets[,1])
   tweets_clean <- gsub("\\#", "", tweets[,1]) 
 }
