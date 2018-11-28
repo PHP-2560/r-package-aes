5ecdc08dea2ffea6769b1a34557f6a67717d93ac
Sarah Fringer <sarah_fringer@brown.edu>
2018-11-28 14:23:07 -0500

    creating package

--- Coffee/R/squares.R
+++ Coffee/R/squares.R
@@ -1,9 +1,11 @@
-#' Squares Data
+#' A square function
 #'
+#' This function allows you to square a variable. 
 #' @param x A numeric value to be squared
 #' @return The square of the input
 #' @export
+#' square()
 
 square <- function(x) {
-   return(x^2)
- }
+  return(x^2)
+}
