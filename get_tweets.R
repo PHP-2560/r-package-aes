a1fe199e50ee543c88f0e4227107a98c2eaa7174
erinbugbee <erin_bugbee@brown.edu>
2018-11-28 14:14:21 -0500

    Add get_tweets.R function

---
+++ get_tweets.R
@@ -0,0 +1,22 @@
+
+get_tweets <- function(word) {
+
+  library(twitteR)
+  library(dplyr)
+  library(purrr)
+  
+  api_key <- "kDhE1qt5Yud7aBaXVCaKN6Zma"
+  api_secret <- "Hd3yLQ3W9OxZO2nwFtgq92EGfzLhMwZ4y67dCbmhD68y98C74u"
+  access_token <- "47221368-YkXjK0S5Bc6zXu5751FMP9LNx9weTkLCjq7UpT2Yo"
+  access_token_secret <- "m0lpA0eVRMhEERvi3vuIfPX7eFCttRWAa4f5b1YGsnYhK"
+  
+  setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+  
+  tweets <- searchTwitter(word, n = 1000)
+  df <- tbl_df(map_df(tweets, as.data.frame))
+  write.csv(df, paste(word, ".csv", sep = ""))
+}
+
+
+
+
