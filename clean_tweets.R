c79539682d2b284c6e8173ec6c8957a046b86bfd
erinbugbee <erin_bugbee@brown.edu>
2018-11-29 11:16:29 -0500

    Improve tweet cleaning

--- clean_tweets.R
+++ clean_tweets.R
@@ -6,4 +6,13 @@ clean_tweets <- function(file) {
   tweets <- select(tweets, text)
   tweets_clean <- gsub("\\d","", tweets[,1])
   tweets_clean <- gsub("\\#", "", tweets_clean) 
+  tweets_clean <- gsub("https(.*)*$", "", tweets_clean)
+  #Remove mentions here
+  #Remove "RT"
+  
+  tweets_clean <- gsub("[[:punct:]]", "", tweets_clean) 
+  tweets_clean <- iconv(tweets_clean, "latin1", "ASCII", sub="") 
+  tweets_clean <- gsub("\\n", "", tweets_clean) 
+  tweets_clean <- tolower(tweets_clean)
+
 }
