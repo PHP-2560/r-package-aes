39b61a0248496f842f86ab768b3ee2cd4c76f38a
erinbugbee <erin_bugbee@brown.edu>
2018-12-05 15:06:04 -0500

    Add get_wordcloud.R function

---
+++ coffee/R/make_wordcloud.R
@@ -0,0 +1,18 @@
+#' Making a Word Cloud
+#'
+#' This function allows you to make a word cloud given a .csv file and the number of words for the word cloud.
+#' @param file A .csv file
+#' @param num_words The number of words in the word cloud, default is 100
+#' @keywords wordcloud
+#' @export
+#' @examples
+#' make_wordcloud()
+make_wordcloud <- function(file, num_words = 100) {
+  library(tm)
+  library(SnowballC)
+  library(wordcloud)
+  words <- read.csv("clean_starbucks_tweets.csv")
+  corpus <- Corpus(VectorSource(words$x))
+  
+  wordcloud(corpus, max.words = 100)
+}
