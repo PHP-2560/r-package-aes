7e46cb0523263fe0b64bf09ac853c12134c934f1
Sarah Fringer <sarah_fringer@brown.edu>
2018-12-09 16:43:22 -0500

    function to make the bar graph

---
+++ make_bargraph.R
@@ -0,0 +1,35 @@
+#' Making a Bar Graph
+#'
+#' This function allows you to make a word cloud given a .csv file and the number of words for the word cloud.
+#' @param file A .csv file
+#' @keywords wordcloud
+#' @export
+#' @examples
+#' make_wordcloud()
+make_wordcloud <- function(file) {
+  library(ggplot2)
+  library(dplyr)
+  library(tidyr)
+  library(tidytext)
+  
+  options(stringsAsFactors = FALSE)
+  tweets <- read.csv(file)
+  # get a list of words
+  tweet_words <- tweets %>%
+    dplyr::select(word) %>%
+    unnest_tokens(word, x)
+  
+  # plot the top 15 words
+  tweet_words %>%
+    count(word, sort = TRUE) %>%
+    top_n(15) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(x = word, y = n)) +
+    geom_col() +
+    xlab(NULL) +
+    coord_flip() +
+    labs(x = "Count",
+         y = "Unique words",
+         title = "Count of unique words found in tweets") + theme_bw()
+  ## Selecting by n
+}
