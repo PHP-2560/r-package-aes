d9aa8023d7669a55aa7dad3eeb7f1c98183f0332
Sarah Fringer <sarah_fringer@brown.edu>
2018-12-11 21:04:18 -0500

    changed the function

--- coffee/R/make_bar_graph.R
+++ coffee/R/make_bar_graph.R
@@ -11,13 +11,14 @@
 make_bar_graph <- function(file) {
   library(tidytext)
   library(ggplot2)
+  library(dplyr)
   tweets_tidy <- unnest_tokens(readr::read_csv(file), word, x)
   tt <- tweets_tidy %>% filter(!nchar(word) < 3) %>% anti_join(stop_words)
   
   tweets_bing <- tt %>%
     inner_join(get_sentiments("bing"))
   
-  bing_plot <- tweets_bing %>%
+  tweets_bing %>%
     group_by(sentiment) %>%
     summarise(word_count = n()) %>%
     ungroup() %>%
@@ -28,8 +29,7 @@ make_bar_graph <- function(file) {
     guides(fill = FALSE) + #Turn off the legend
     labs(x = NULL, y = "Word Count") +
     scale_y_continuous(limits = c(0, 500)) + #Hard code the axis limit
-    ggtitle("Tweets NRC Sentiment") +
+    ggtitle("Tweets Sentiment") +
     coord_flip()
   
-  plot(bing_plot)
 }
